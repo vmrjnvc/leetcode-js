1092f5d173999ed4a162534b308d3574b77f450a
vuk.marjanovic <vmrjnvc@gmail.com>
2025-09-08 23:35:56 +0200

    remove-duplicates-from-sorted-list: solved

--- problems/83.remove-duplicates-from-sorted-list.js
+++ problems/83.remove-duplicates-from-sorted-list.js
@@ -26,5 +26,14 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
-
+    const dummy = new ListNode();
+    dummy.next = head;
+    while (head) {
+        if (head.val === head.next?.val) {
+            head.next = head.next.next;
+        } else {
+            head = head.next || null;
+        }
+    }
+    return dummy.next
 };
